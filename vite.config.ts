0f3405c5690cb159d3badbb022626c24e271b184
LazaroOlix <hyperomega26@gmail.com>
2025-12-18 16:28:08 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: './', // MUITO IMPORTANTE para deploy no Render
+  base: './', // ESSENCIAL para deploy no Render
 });
